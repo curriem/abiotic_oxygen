34a9eb341aefae8d1affaaf217082af7702ed3e7
Miles Currie <mcurr@D-10-19-21-211.dhcp4.washington.edu>
2019-07-03 14:32:29 -0700

    pt grid

--- vary_cold_trap/scripts/make_pt_fls.py
+++ vary_cold_trap/scripts/make_pt_fls.py
@@ -1,12 +1,12 @@
 import project_tools
 import numpy as np
 
-o2_abundance = 0.2
-loc = 'upper'
-cold_traps = np.logspace(-3, 0, 30)
-for cold_trap in cold_traps:
-    fl_name = 'cold_trap_%s.pt' % str(cold_trap).replace('.', '_')
-    project_tools.make_pt_fl(loc, o2_abundance, '../pt_fls/' + fl_name, cold_trap)
+cold_traps = np.arange(0, 1., 0.1)
+o2_factors = np.arange(0, 0.5, 0.05)
 
-# add an atmosphere with no oxygen
-project_tools.make_pt_fl(o2_loc='mixed', o2_mixing_ratio=0., place='../pt_fls/no_oxygen.pt')
+for cold_trap in cold_traps:
+    for o2_factor in o2_factors:
+
+
+        fl_name = 'trop_%s_o2_%s.pt' % (str(cold_trap).replace('.', '_'), str(o2_factor).replace('.', '_'))
+        project_tools.make_pt_fl('upper', o2_factor, '../pt_fls/' + fl_name, cold_trap)
